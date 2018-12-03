fa82e44dc17adffc48da86a41bbc7e5802f5728c
shannongamby <shannongamby@hotmail.com>
2018-12-03 14:26:57 +0000

    client can withdraw money

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,10 +1,20 @@
 require 'account'
 
 describe Account do
+
   context 'depositing money' do
     it 'should add the deposited money to the balance' do
       subject.deposit(100)
       expect(subject.balance).to eq 100
     end
   end
+
+  context 'withdrawing money' do
+    it 'should remove a specified amount from the balance' do
+      subject.deposit(100)
+      subject.withdraw(100)
+      expect(subject.balance).to eq 0
+    end
+  end
+
 end
